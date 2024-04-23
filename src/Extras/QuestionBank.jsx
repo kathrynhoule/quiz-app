0751e047e2faaea0e9bacb40fcc2dea88c000d0b
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-23 18:17:12 -0400

    adds placeholder data to questions array

---
+++ src/Extras/QuestionBank.jsx
@@ -0,0 +1,30 @@
+export const questions = [
+     {
+          id: 1,
+          prompt: "What is 1 + 5?",
+          optionA: "7",
+          optionB: "4",
+          optionC: "6",
+          optionD: "3",
+          answer: "optionC",
+     },
+     {
+          id: 2,
+          prompt: "What is 1 + 5?",
+          optionA: "7",
+          optionB: "4",
+          optionC: "6",
+          optionD: "3",
+          answer: "optionC",
+     },
+     {
+          id: 3,
+          prompt: "What is 1 + 5?",
+          optionA: "7",
+          optionB: "4",
+          optionC: "6",
+          optionD: "3",
+          answer: "optionC",
+     },
+
+];

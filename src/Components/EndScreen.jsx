8fe1b38523509635dc9e574acebc34ccfd8004ee
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-24 16:35:43 -0400

    adds context to end screen and adds function to restart the quiz

--- src/Components/EndScreen.jsx
+++ src/Components/EndScreen.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { QuizContext } from "../Extras/Contexts";
+import { questions } from "../Extras/QuestionBank";
 
 function EndScreen() {
+     const { score, setScore, setGameState } = useContext(QuizContext);
+
+     const restartQuiz = () => {
+          setScore(0);
+          setGameState("menu");
+     };
+
      return (
           <div className="end-screen">
-               <h1>End Screen</h1>
+               <h1>Quiz Completed!</h1>
+               <h3> {score} / {questions.length} </h3>
+               <button onClick={restartQuiz}> Retry Quiz </button>
           </div>
      );
 }

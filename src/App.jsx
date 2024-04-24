361d23bc154a368bf16ec96fd742a82953134ac5
kathrynhoule <kathrynhoule@torontomu.ca>
2024-04-24 16:16:50 -0400

    adds buttons for answer options to questions and function to keep track of score

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,13 @@ import { QuizContext } from "./Extras/Contexts";
 
 function App() {
      const [gameState, setGameState] = useState("menu");
+     const [score, setScore] = useState(0);
 
      return (
           <div className="app">
                <h1>Quiz App</h1>
 
-               <QuizContext.Provider value={{ gameState, setGameState }}>
+               <QuizContext.Provider value={{ gameState, setGameState, score, setScore }}>
                {gameState === "menu" && <MainMenu />}
                {gameState === "quiz" && <Quiz />}
                {gameState === "endScreen" && <EndScreen />}
--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { QuizContext } from "../Extras/Contexts";
+import {questions} from '../Extras/QuestionBank';
 
 function Quiz() {
+     const { score, setScore } = useContext(QuizContext);
+
+     const [currQuestion, setCurrQuestion] = useState(0);
+     const [optionChosen, setOptionChosen] = useState("");
+
+     const nextQuestion = () => {
+          if (questions[currQuestion].answer === optionChosen) {
+               setScore(prevScore => prevScore + 1);
+          }
+          alert(score);
+          setCurrQuestion(currQuestion + 1);
+     }
+
      return (
           <div className="quiz">
-               <h1>Quiz</h1>
+               <h1>{questions[currQuestion].prompt}</h1>
+               <div className="options">
+                    <button onClick={() => setOptionChosen("A")}>{questions[currQuestion].optionA}</button>
+                    <button onClick={() => setOptionChosen("B")}>{questions[currQuestion].optionB}</button>
+                    <button onClick={() => setOptionChosen("C")}>{questions[currQuestion].optionC}</button>
+                    <button onClick={() => setOptionChosen("D")}>{questions[currQuestion].optionD}</button>
+               </div>
+
+               <button onClick={nextQuestion}>Next Question</button>
           </div>
      );
 }
